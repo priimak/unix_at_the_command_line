46f424004cebfd6d7431073c20b7baf3d086ac4c
Dmitri Priimak <dpriimak@svbio.com>
2014-11-10 18:17:38 -0800

    Initial commit

---
+++ auto.convert.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+while true
+do
+    for f in `find src -type f -name '*.adoc' -newer .adoc.ts`
+    do
+	echo "Regenerating $f"
+	asciidoctor -b html5 \
+	    --destination-dir=target/site/ $f && touch .adoc.ts    
+    done
+    sleep 0.2
+done
